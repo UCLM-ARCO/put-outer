353eec39b1f1582acbf8a27c3a8353c772f58a99
Ana Rubio <Ana.Rubio@uclm.es>
2021-01-11 20:46:49 +0100

    install dependencias script

---
+++ install-dependencies.sh
@@ -0,0 +1,73 @@
+#! /bin/sh
+
+echo "=================================================="
+echo "      pike.esi.uclm.es ----> sources.list"
+echo "=================================================="
+sudo apt-key adv --fetch-keys https://uclm-arco.github.io/debian/key.asc
+echo "deb https://uclm-arco.github.io/debian/ sid main" | sudo tee /etc/apt/sources.list.d/arco.list
+sudo apt update
+
+echo "=================================================="
+echo "            Installing Python3"
+echo "=================================================="
+sudo apt install -y python3
+
+echo "=================================================="
+echo "         Installing Python3 Commodity"
+echo "=================================================="
+sudo apt install python3-commodity
+
+echo "=================================================="
+echo "             Installing ZeroC Ice"
+echo "=================================================="
+sudo apt install python-zeroc-ice36 zeroc-ice36
+
+echo "=================================================="
+echo "            Installing Scone-Wrapper"
+echo "=================================================="
+sudo apt install scone-wrapper
+
+echo "=================================================="
+echo "                Installing PIP"
+echo "=================================================="
+sudo apt install python3-pip
+
+echo "=================================================="
+echo "        Installing networkx Python lib"
+echo "=================================================="
+sudo pip3 install networkx
+
+echo "=================================================="
+echo "                Installing numpy"
+echo "=================================================="
+sudo pip3 install numpy
+
+echo "=================================================="
+echo "          Installing scipy Python lib"
+echo "=================================================="
+sudo pip3 install scipy
+
+echo "=================================================="
+echo "        Installing matplotlib Python lib"
+echo "=================================================="
+sudo pip3 install matplotlib
+
+echo "=================================================="
+echo "            Installing lxml Python lib"
+echo "=================================================="
+sudo pip3 install lxml
+
+echo "=================================================="
+echo "          Installing shapely Python lib"
+echo "=================================================="
+sudo pip3 install shapely
+
+echo "=================================================="
+echo "            Installing nose Python lib"
+echo "=================================================="
+sudo pip3 install nose
+
+echo "=================================================="
+echo "           Installing tkinter python lib"
+echo "=================================================="
+sudo apt install python3-tk
--- sbcl.lisp
+++
@@ -1,4 +0,0 @@
-(load "/usr/share/scone/scone-start.lisp")
-(load "scone-knowledge.d/01-general.lisp")
-(load "scone-knowledge.d/02-statuses.lisp")
-(load "scone-knowledge.d/03-navigability.lisp")
