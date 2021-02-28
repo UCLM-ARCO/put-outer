3e800da694c6a6acbb297991b24ad7b6b7ba2a83
Ana Rubio <Ana.Rubio@uclm.es>
2021-02-28 17:08:24 +0100

    fix, ex. 1: knowledge paths for sbcl

--- examples/01/scone-start.lisp
+++ examples/01/scone-start.lisp
@@ -2,9 +2,9 @@
 (scone "1.0.0")
 (load-kb "core.lisp")
 
-(load "../scone-knowledge.d/01-general.lisp")
-(load "../scone-knowledge.d/02-statuses.lisp")
-(load "../scone-knowledge.d/03-navigability.lisp")
+(load "../../scone-knowledge.d/01-general.lisp")
+(load "../../scone-knowledge.d/02-statuses.lisp")
+(load "../../scone-knowledge.d/03-navigability.lisp")
 
 (load "model.lisp")
 (load "init.lisp")
