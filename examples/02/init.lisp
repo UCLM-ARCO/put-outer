2a4272a005391617f2d476a6e4e49de34de10b81
Ana Rubio <Ana.Rubio@uclm.es>
2021-02-28 17:12:31 +0100

    add setup for case 2

---
+++ examples/02/init.lisp
@@ -0,0 +1,10 @@
+;; Config of the case of use
+(new-is-a {D1} {closed element})
+(new-is-a {D2} {closed element})
+(new-is-a {D3} {closed element})
+(new-is-a {D4} {closed element})
+
+(new-is-a {L2_STATE} {off})
+(new-is-a {L3_STATE} {off})
+(new-is-a {L1_STATE} {inoperative})
+(new-is-a {L4_STATE} {off})
