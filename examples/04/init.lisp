de0292c2e3d5a6bdba9f4854a9d8ed4326e3277b
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2021-03-05 20:00:21 +0100

    case 4 Makefile + config

---
+++ examples/04/init.lisp
@@ -0,0 +1,9 @@
+;; Config of the case of use
+(new-is-a {D1} {closed element})
+(new-is-a {D2} {open element})
+(new-is-a {D3} {closed element})
+
+(new-is-a {L2} {off})
+(new-is-a {L3} {off})
+(new-is-a {L1.1} {off})
+(new-is-a {L1.2} {off})
